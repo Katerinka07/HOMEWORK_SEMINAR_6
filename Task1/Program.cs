f52f6d5db5cc7b7cdc37e23e2eb82f7fe12a4b3e
Ekaterina <kat-erina-07@mail.ru>
2023-07-01 12:07:03 +0400

    Count numbers

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = InputNum($"Введите {i + 1}-й элемент: ");
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine();
+}
+int CountNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count ++;
+    }
+    return count;
+}
+int size = InputNum("Введите количество элементов: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray);
+PrintArray(myArray);
+int res = CountNum(myArray);
+Console.WriteLine($"Количество чисел больше 0: {res}");
---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double InputNum(string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine()!);
+}
+double[] IntersPoint(double num1, double num2, double num3, double num4)
+{
+    double resX = (num3 - num1) / (num2 - num4);
+    double resY = num4 * resX + num3;
+    double[] array = new double[2];
+    array[0] = resX;
+    array[1] = resY;
+    return array;
+}
+void PrintArray(double[] array)
+{
+    Console.Write("(");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + ")");
+}
+double b1 = InputNum("Введите значение b1: ");
+double k1 = InputNum("Введите значение k1: ");
+double b2 = InputNum("Введите значение b2: ");
+double k2 = InputNum("Введите значение k2: ");
+double[] res = IntersPoint(b1, k1, b2, k2);
+PrintArray(res);
+Console.WriteLine();
+if (res[0] != res[1]) Console.WriteLine("Прямые не пересекаются");
